52e5be2776c87d2be86198fe3044d9e5c633a96c
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-02 01:13:10 +0000

    second commit

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍️👍️Assertion Passed: " + actual + "===" + expected);
+    console.log(`👍️👍️Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("⛔️⛔️Assertion Failed:" + actual + "!== " + expected);
+    console.log(`⛔️⛔️Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
