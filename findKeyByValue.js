6c0e0c26bd293e0b779c280857c47b1cb27735f8
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-03 23:37:03 +0000

    commit find Key

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍️👍️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️Assertion Failed: ${actual} !== ${expected}`);
+}
+};
+const findKeyByValue = function(object1,value) {
+  for (const index in object1)
+  {
+    if(object1[index]===value){
+     return index;
+    }
+  }
+};
+
+
+
+
+// Testcases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
