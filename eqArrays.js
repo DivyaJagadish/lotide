89e286967ce95ac2940868bd529ee46fc654ade8
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-08 02:41:23 +0000

    EqArrays using recursion

--- eqArrays.js
+++ eqArrays.js
@@ -8,7 +8,12 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(array1,array2) {
   if (array1.length === array2.length) { // checking whether lengths are equal
     for (let i = 0; i < array2.length; i++) {
-      if (array1[i] !== array2[i]) {
+      if(Array.isArray(array1[i])) {
+        const check = eqArrays(array1[i],array2[i]);
+        if(check === false){
+          return false;
+        }
+      }else if (array1[i] !== array2[i]) {
         return false;
       }
     } return true;
@@ -18,4 +23,10 @@ const eqArrays = function(array1,array2) {
 };
 
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false);
+assertEqual(eqArrays([1, [2, 3], 4, 5], [1, [2, 3], 4, 5]),true);
+assertEqual(eqArrays([1, [2, [3,2,[1]], 4, 5],6],[1, [2, [3,2,[1]], 4, 5],6]),true);
+assertEqual(eqArrays([1, [2, [3,2,[1,[2,3]]], 4, 5],6],[1, [2, [3,2,[1,[2,3]]], 4, 5],6]),true);
+
+
+
 
