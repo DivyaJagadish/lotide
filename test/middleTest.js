705ab8824eae90479291f89a801082c08d10e134
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-08 22:12:31 +0000

    Tail and middle using mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,11 @@
-const middle =require("../middle")
-const assertArraysEqual =require("../assertArraysEqual")
+const middle =require("../middle");
+const assert = require("chai").assert;
+describe("#middle",()=>{
+  it ("return[2] on passing [1,2,3]",()=>{
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+  it ("return [2,3] on passing [1,2,3,4]",()=>{
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  })
+});
 
-// Test cases
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,20 @@
 const tail = require("../tail");
-const assertEqual =require("../assertEqual");
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const words1= ["Yo Yo", "Lighthouse", "Labs"];
-const result =tail(words1);
-assertEqual(result.length, 2)
+const assert = require("chai").assert;
+
+describe("#tail",()=>{
+  it ("return [2,3,4,5] with input [1,2,3,4,5]",()=>{
+    const result = tail([1,2,3,4,5]);
+    assert.deepEqual(result,[2,3,4,5]);
+  });
+  it ("return ['b','c','d'] on passing ['a','b','c','d']",()=>{
+  assert.deepEqual(tail(['a','b','c','d']),['b','c','d'])
+  });
+  it ("The original size of array shouldnot be changed",()=>{
+    const words= ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.deepEqual(words.length, 3)
+  });
+
+});
+
 
